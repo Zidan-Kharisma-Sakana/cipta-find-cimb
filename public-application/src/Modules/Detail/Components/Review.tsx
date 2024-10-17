7116c89dea317ab6f92425ca170b1457277d9380
daryfauzan <daryfauza@gmail.com>
2024-10-17 11:26:25 +0700

    feat: parameterized components

--- public-application/src/Modules/Detail/Components/Review.tsx
+++ public-application/src/Modules/Detail/Components/Review.tsx
@@ -1,8 +1,15 @@
-const Review = () => {
+import React from "react"
+
+interface ReviewProp {
+    rating: number;
+    review: String
+}
+
+const Review: React.FC<ReviewProp> = ({rating, review}) => {
     return (
         <div className="py-2 border-t">
-            <p>⭐4</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet sequi, sunt magni voluptatibus nisi tempora nihil molestias pariatur alias ut eius nobis. A nemo, recusandae exercitationem voluptas amet consectetur eligendi.</p>
+            <p>⭐{rating}</p>
+            <p>{review}</p>
         </div>
     )
 }

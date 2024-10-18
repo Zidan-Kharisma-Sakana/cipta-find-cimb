81965a60bb398fc8ce245f9c83e1b5f660110fa9
cakrapand <cakrapand@gmail.com>
2024-10-18 11:36:17 +0700

    fix: datatables

--- admin-application/resources/views/branch/index.blade.php
+++ admin-application/resources/views/branch/index.blade.php
@@ -92,47 +92,47 @@
                 },
                 columns: [{
                         data: 'id',
-                        name: 'Branch.id',
+                        name: 'id',
                     },
                     {
                         data: 'name',
-                        name: 'Branch.name',
+                        name: 'name',
                     },
                     {
                         data: 'type',
-                        name: 'Branch.type',
+                        name: 'type',
                     },
                     {
                         data: 'address',
-                        name: 'Branch.address',
+                        name: 'address',
                     },
                     // {
                     //     data: 'city',
-                    //     name: 'Branch.city',
+                    //     name: 'city',
                     // },
                     // {
                     //     data: 'province',
-                    //     name: 'Branch.province',
+                    //     name: 'province',
                     // },
                     // {
                     //     data: 'cp',
-                    //     name: 'Branch.cp',
+                    //     name: 'cp',
                     // },
                     {
                         data: 'opHour',
-                        name: 'Branch.opHour',
+                        name: 'opHour',
                     },
                     {
                         data: 'queue',
-                        name: 'Branch.queue',
+                        name: 'queue',
                     },
                     // {
                     //     data: 'rate',
-                    //     name: 'Branch.rate',
+                    //     name: 'rate',
                     // },
                     {
                         data: 'is_deleted',
-                        name: 'Branch.is_deleted',
+                        name: 'is_deleted',
                     },
                     {
                         targets: -1,
